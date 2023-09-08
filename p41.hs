5ff1cc5f84341506b6a7262b833ffda9a9e44fcb
Guilherme Biem <guibiem2357@gmail.com>
2023-09-08 11:56:22 -0400

    #41

--- p41.hs
+++ p41.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE ViewPatterns #-}
 import Data.List
-
+import qualified Codec.Picture as digits
+--- Notice that all 9-digit pandigital numbers are divisible by 3, because their sum of digits is divisible by 3. 
+--- So are all 8-digit pandigital numbers. So the greatest prime must be 7 digits.
+--- This algorithm goes from the largest 7-digit pandigital number down and returns the first prime.
 main::IO()
-main = print $  filter (isPrime) [n| n<-[987654321,987654320..1], isPandigital n]
+main = print $  head $ filter (isPrime) [n| n<-[7654321,7654320..1], isPandigital n]
         where isPrime k = if k > 1 then null [ x | x <- [2..intSqrt k], k `mod` x == 0] else False
               intSqrt::Int -> Int
               intSqrt = ceiling . sqrt . fromIntegral
